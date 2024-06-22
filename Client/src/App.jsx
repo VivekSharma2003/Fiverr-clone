4ec019036c693a59cf7077651c140c758b656ad9
Vivek Sharma <viveksh1952003@gmail.com>
2024-06-22 19:31:56 +0530

    added samesite

---
+++ Client/src/App.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import Footer from "./components/footer/Footer"
+import Gigs from "./pages/gigs/Gigs";
+import Gig from "./pages/gig/Gig";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Orders from "./pages/orders/Orders";
+import MyGigs from "./pages/myGigs/MyGigs";
+import Add from "./pages/add/Add";
+import Messages from "./pages/messages/Messages";
+import Message from "./pages/message/Message";
+import Pay from "./pages/pay/Pay";
+import Success from "./pages/success/Success";
+import "./app.scss"
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+function App() {
+
+  const queryClient = new QueryClient()
+
+  const Layout = () => {
+    return (
+      <div className="app">
+         <QueryClientProvider client={queryClient}>
+        <Navbar />
+        <Outlet />
+        <Footer />
+        </QueryClientProvider>
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout/>,
+      children:[
+        {
+          path:"/",
+          element: <Home/>
+        },
+        {
+          path:"/gigs",
+          element: <Gigs/>
+        },
+        {
+          path:"/gig/:id",
+          element: <Gig/>
+        },
+        {
+          path:"/orders",
+          element: <Orders/>
+        },
+        {
+          path:"/myGigs",
+          element: <MyGigs/>
+        },
+        {
+          path:"/add",
+          element: <Add/>
+        },
+        {
+          path:"/messages",
+          element: <Messages/>
+        },
+        {
+          path:"/message/:id",
+          element: <Message/>
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/register",
+          element: <Register />,
+        },
+        {
+          path: "/pay/:id",
+          element: <Pay />,
+        },
+        {
+          path: "/success",
+          element: <Success />,
+        },
+      ]
+    },
+  ]);
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
